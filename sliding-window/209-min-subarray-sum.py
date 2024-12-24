92ed2105ef41f3744c9d2768e54f53e1afa04f90
Isabel Faulds <fauldsisabel@outlook.com>
2024-12-24 11:53:38 -0800

    leetstream second method

--- sliding-window/209-min-subarray-sum.py
+++ sliding-window/209-min-subarray-sum.py
@@ -1,3 +1,4 @@
+
 # o of n solution
 
 class Solution:
@@ -19,5 +20,36 @@ class Solution:
         min_length = 0 if min_length == float('inf') else min_length
         return min_length                        
 
+# o n log n
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        cumulative = [0] * ( n + 1 )
+        cumulative_counter = 0
+        for i in range(n):
+            cumulative_counter += nums[i]
+            cumulative[i + 1] = cumulative_counter
+        
+        # binary search on each indice
+        min_length = float('inf')
+        for i in range(len(cumulative)):
+            c_target = cumulative[i] + target
+
+            left , right = i , n
+            while left <= right:
+                mid = ( left + right ) // 2
+                if cumulative[mid] < c_target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            if left < len(cumulative):
+                min_length = min(left - i , min_length)
+
             
-# o log n solution
+
+        min_length = 0 if min_length == float('inf') else min_length
+        return min_length      
+
+
